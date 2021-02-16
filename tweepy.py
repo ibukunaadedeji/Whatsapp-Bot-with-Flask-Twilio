ca943eb922fbff2257e002bb4e8f3cc18b2486ca
ibukunoluwa <ibukunaadedeji@gmail.com>
2021-02-17 00:00:12 +0100

    API status

--- tweepy.py
+++ tweepy.py
@@ -10,3 +10,5 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 timeline = api.home_timeline()
 for tweet in timeline:
     print(f"{tweet.user.name} said {tweet.text}")
+
+api.update_status("Test tweet from Tweepy Python")
